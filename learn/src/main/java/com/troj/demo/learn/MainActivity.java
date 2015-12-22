5ef351a11c205a1627eeb44074b6a04925d9eba8
siren186 <siren186@163.com>
2015-12-22 10:55:17 +0800

    + [learn] 接入Crasheye崩溃平台

--- learn/src/main/java/com/troj/demo/learn/MainActivity.java
+++ learn/src/main/java/com/troj/demo/learn/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.xsj.crasheye.Crasheye;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
@@ -14,10 +16,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        initCrasheye();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         onCreateImpl();
+    }
+
+    private void initCrasheye() {
+        Crasheye.init(this, "eaa37bd0");
+        Crasheye.setFlushOnlyOverWiFi(true);
     }
 
     private void onCreateImpl() {
