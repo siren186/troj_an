888c659e6538aca3701a3610ddff76ffa04d1143
siren186 <siren186@163.com>
2015-12-22 10:31:12 +0800

    * [learn] 引入ButterKnife

--- learn/src/main/java/com/troj/demo/learn/MainActivity.java
+++ learn/src/main/java/com/troj/demo/learn/MainActivity.java
@@ -2,12 +2,25 @@ package com.troj.demo.learn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.activity_main_tv1)
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        onCreateImpl();
+    }
+
+    private void onCreateImpl() {
+        tv1.setText(R.string.helloworld);
     }
 }
