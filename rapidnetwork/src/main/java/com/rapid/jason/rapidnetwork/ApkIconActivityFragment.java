2218a21fd88c2ce7d214f8253863487c259d3d96
钟华健 <915874362@qq.com>
2016-01-10 14:36:38 +0800

    * volley 快速下载 添加图片listview

--- rapidnetwork/src/main/java/com/rapid/jason/rapidnetwork/ApkIconActivityFragment.java
+++ rapidnetwork/src/main/java/com/rapid/jason/rapidnetwork/ApkIconActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -12,12 +14,33 @@ import android.view.ViewGroup;
  */
 public class ApkIconActivityFragment extends Fragment {
 
+    private ListView lvCard = null;
+    private ArrayAdapter<String> mAdapter = null;
+
+    private static final String[] strs = new String[] {
+        "first", "second", "third", "fourth", "fifth"
+    };
+
     public ApkIconActivityFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_apk_icon, container, false);
+        View view = null;
+        view = inflater.inflate(R.layout.fragment_apk_icon, container, false);
+
+        lvCard = (ListView) view.findViewById(R.id.lv_card);
+        mAdapter = new ArrayAdapter<String>(this.getActivity(), R.layout.apkicon_image_card_layout, strs);
+        if (mAdapter != null && lvCard != null) {
+            lvCard.setAdapter(mAdapter);
+        }
+
+        return view;
     }
 }
