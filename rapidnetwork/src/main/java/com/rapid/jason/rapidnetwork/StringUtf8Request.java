b78a42edfeac27d20821d72e83945480ec405477
jz <915874362@qq.com>
2016-01-11 22:07:14 +0800

    * volley 快速下载 修改获取图片地址解析

---
+++ rapidnetwork/src/main/java/com/rapid/jason/rapidnetwork/StringUtf8Request.java
@@ -0,0 +1,36 @@
+package com.rapid.jason.rapidnetwork;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import java.io.UnsupportedEncodingException;
+
+public class StringUtf8Request extends Request<String> {
+    private final Response.Listener<String> mListener;
+
+    public StringUtf8Request(int method, String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        super(method, url, errorListener);
+        this.mListener = listener;
+    }
+
+    public StringUtf8Request(String url, Response.Listener<String> listener, Response.ErrorListener errorListener) {
+        this(0, url, listener, errorListener);
+    }
+
+    protected void deliverResponse(String response) {
+        this.mListener.onResponse(response);
+    }
+
+    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+        String parsed;
+        try {
+            parsed = new String(response.data, "UTF-8");
+        } catch (UnsupportedEncodingException var4) {
+            parsed = new String(response.data);
+        }
+
+        return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response));
+    }
+}
