3b784f29bfb0091e082ad71f15c09e76ee0c0d9a
钟华健 <915874362@qq.com>
2016-02-13 16:41:02 +0800

    * 完善基础适配器

--- rapidnetwork/src/main/java/com/rapid/jason/rapidnetwork/ListWordCard/ListWordCardsAdapter.java
+++ rapidnetwork/src/main/java/com/rapid/jason/rapidnetwork/ListWordCard/ListWordCardsAdapter.java
@@ -1,7 +1,6 @@
 package com.rapid.jason.rapidnetwork.ListWordCard;
 
 import android.content.Context;
-import android.view.View;
 import android.widget.TextView;
 
 import com.rapid.jason.rapidnetwork.ListViewUtil.BaseCardsAdapter;
