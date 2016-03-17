e16dbd02c9ec87cb6514cf52334717f4f832d1d8
siren186 <siren186@163.com>
2016-03-17 16:48:12 +0800

    * learn 连按2次返回键退出应用

--- learn/src/main/java/com/troj/demo/learn/ui/main/HomeActivity.java
+++ learn/src/main/java/com/troj/demo/learn/ui/main/HomeActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.troj.demo.learn.R;
 
@@ -14,6 +15,7 @@ import butterknife.ButterKnife;
 
 public class HomeActivity extends Activity {
 
+    private long mBackKeyPressedTime = 0;
     @Bind(R.id.activity_home_tv_test)
     TextView mTvTest;
 
@@ -27,6 +29,18 @@ public class HomeActivity extends Activity {
         String str = intent.getStringExtra("tv_test");
         if (!TextUtils.isEmpty(str)) {
             mTvTest.setText(str);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if((System.currentTimeMillis() - mBackKeyPressedTime) > 2000){
+            Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            mBackKeyPressedTime = System.currentTimeMillis();
+        } else {
+            super.onBackPressed();
+            finish();
+            System.exit(0);
         }
     }
 }
